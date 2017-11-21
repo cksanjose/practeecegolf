e3a20ddbeea36e257475efae4b3cf1e7a8e3a11f
cesar.sanjose <cksanjose@gmail.com>
2017-11-20 19:35:13 -0600

    Add page not found and route module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Practeece Golf';
+  title = 'Welcome To Practeece Golf';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,23 +7,35 @@ import { AppComponent } from './app.component';
 import { PracticeComponent } from './practice/practice.component';
 import { SkillLevelComponent } from './skill-level/skill-level.component';
 import { GolfHoleComponent } from './golf-hole/golf-hole.component';
-import {GolfHoleService} from "./golf-hole/golf-hole.service";
+import { GolfHoleService } from "./golf-hole/golf-hole.service";
+import { PathNotFoundComponent } from './shared/path-not-found/path-not-found.component';
+import { RouterModule, Routes } from "@angular/router";
+
+const appRoutes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'practice', component: PracticeComponent },
+  { path: '**', component: PathNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     PracticeComponent,
     SkillLevelComponent,
-    GolfHoleComponent
+    GolfHoleComponent,
+    PathNotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true }),
   ],
   providers: [
     GolfHoleService
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
