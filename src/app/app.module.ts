b4d67e0aa2da1eb38dd53e5961675a12732e8d27
cesar.sanjose <cksanjose@gmail.com>
2017-11-22 15:40:47 -0600

    Added Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { GolfHoleComponent } from './golf-hole/golf-hole.component';
 import { GolfHoleService } from "./golf-hole/golf-hole.service";
 import { PathNotFoundComponent } from './shared/path-not-found/path-not-found.component';
 import { RouterModule, Routes } from "@angular/router";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
 
 const appRoutes: Routes = [
   { path: '', component: AppComponent },
@@ -30,6 +31,7 @@ const appRoutes: Routes = [
     FormsModule,
     HttpModule,
     RouterModule.forRoot(appRoutes, { enableTracing: true }),
+    NoopAnimationsModule
   ],
   providers: [
     GolfHoleService
