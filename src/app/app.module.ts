19a4f94798609900996f6f6f4b275283ba9ee574
cesar.sanjose <cksanjose@gmail.com>
2018-01-07 14:41:13 -0600

    Cleanup dep modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
@@ -35,11 +36,12 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
     FormsModule,
     HttpClientModule,
     RouterModule.forRoot(appRoutes, { enableTracing: true }),
-    ReactiveFormsModule,
-    MaterialModule
+    ReactiveFormsModule
   ],
   providers: [
     GolfHoleService,
