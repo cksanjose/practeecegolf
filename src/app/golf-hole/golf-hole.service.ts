587601f0e90e086180c05e8e4fd5a30abb627a6e
cesar.sanjose <cksanjose@gmail.com>
2018-03-10 07:59:49 -0600

    Add function to return a golf hole by skill level

--- src/app/golf-hole/golf-hole.service.ts
+++ src/app/golf-hole/golf-hole.service.ts
@@ -6,10 +6,20 @@ import { GolfHole } from "../shared/golfHole";
 @Injectable()
 export class GolfHoleService {
   private _dataUrl = "./golf/data/";
+  private currentGolfHole: GolfHole;
 
   constructor(private _http: HttpClient) { }
 
   getGolfHoles(): Observable<GolfHole[]> {
-    return this._http.get<GolfHole[]>(this._dataUrl + "holes.json")
+    return this._http.get<GolfHole[]>(this._dataUrl + "holes.json");
+  }
+
+  getGolfHole(skillLevel: string): GolfHole | null {
+    this.getGolfHoles().subscribe(holes => {
+        const filteredGolfHoles = holes.filter(g => g.skill === skillLevel);
+        const idx = Math.floor(Math.random() * filteredGolfHoles.length);
+        this.currentGolfHole = filteredGolfHoles[idx];
+    });
+    return this.currentGolfHole;
   }
 }
