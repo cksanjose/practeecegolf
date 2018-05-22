997673627190d278ed2f43173dfabf61d146a571
cesar.sanjose <cksanjose@gmail.com>
2018-05-21 20:04:45 -0500

    Update shot results with skill level array

--- src/app/shared/shotOption.ts
+++ src/app/shared/shotOption.ts
@@ -1,4 +1,5 @@
 export class ShotOption {
   par: number;
   shotCount: number;
+  skillLevel: number[];
 }
--- src/app/shot-result/shot-result-engine/par3-shot-result-engine.ts
+++ src/app/shot-result/shot-result-engine/par3-shot-result-engine.ts
@@ -3,7 +3,8 @@ import { ShotResult } from '../../shared/shotResult';
 
 export class Par3ShotResultEngine extends BaseShotResultEngine {
   public determineShotResult(skillLevel: number, swingCount: number): ShotResult {
-      this.filteredShotResults = this.shotResults.filter(s => s.option.filter(o => o.par === 3 && o.shotCount === swingCount));
+      this.filteredShotResults = this.shotResults.filter(s => s.option.filter(o => o.par === 3
+            && o.shotCount === swingCount && o.skillLevel.indexOf(skillLevel) >= 0));
       const idx = Math.floor(Math.random() * this.filteredShotResults.length);
       this.shotResult = this.filteredShotResults[idx];
       return this.shotResult;
