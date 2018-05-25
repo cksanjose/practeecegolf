476530070099e7c4f0fced7bb1d4157685ac4873
Cesar San Jose <SANJCES@chrobinson.com>
2018-05-25 15:57:46 -0500

    Par 5 result for advanced players

--- src/app/shot-result/shot-result-engine/par5-shot-result-engine.ts
+++ src/app/shot-result/shot-result-engine/par5-shot-result-engine.ts
@@ -3,9 +3,17 @@ import { ShotResult } from '../../shared/shotResult';
 
 export class Par5ShotResultEngine extends BaseShotResultEngine {
     public determineShotResult(skillLevel: number, swingCount: number): ShotResult {
-      this.filteredShotResults = this.shotResults.filter(s => s.option.filter(o => o.par === 5 && o.shotCount === swingCount));
-      const idx = Math.floor(Math.random() * this.filteredShotResults.length);
-      this.shotResult = this.filteredShotResults[idx];
+
+      if (skillLevel === 3 && swingCount >= 4) {
+          // advanced players with swing count of 4 or greater, gets ball on green result
+          this.shotResult = this.shotResults.find(s => s.shotResultId === 4);
+      } else {
+          this.filteredShotResults = this.shotResults.filter(s => s.option.filter(o => o.par === 5
+            && o.shotCount === swingCount && o.skillLevel.indexOf(skillLevel) >= 0));
+          const idx = Math.floor(Math.random() * this.filteredShotResults.length);
+          this.shotResult = this.filteredShotResults[idx];
+      }
+
       return this.shotResult;
     }
 }
